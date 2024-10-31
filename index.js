cc849644d5090f87ffe79b2b316ef54f74b9dd13
daniel_mason <danmasson0@gmail.com>
2024-10-31 18:56:31 -0400

    update server with route to request header parser

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + "/views/timestamp.html");
+});
+
+app.get("/request-header-parser", function (req, res) {
+  res.sendFile(__dirname + "/views/request-header-parser.html");
+});
+
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   console.log({ greeting: "hello API" });
