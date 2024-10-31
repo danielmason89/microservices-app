d2d0c72bd09b9693df5c0b84733587688973dc24
daniel_mason <danmasson0@gmail.com>
2024-10-31 17:30:08 -0400

    add the ability to receive display a timstaps if a string query is given or if it is left blank

--- index.js
+++ index.js
@@ -25,10 +25,27 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api/", function (req, res) {
+  let now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
+
 app.get("/api/:date_string", function (req, res) {
   let dateString = req.params.date_string;
   console.log(dateString);
-  res.json({ error: "Invalid Date" });
+  let passedInValue = new Date(dateString);
+
+  if (passedInValue == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: passedInValue.getTime(),
+      utc: passedInValue.toUTCString(),
+    });
+  }
 });
 
 // Listen on port set in environment variable or default to 3000
