b9b4502c693b8fa7f9ea40a7b0f1fe44b29dc7dc
daniel_mason <danmasson0@gmail.com>
2024-11-01 14:00:33 -0400

    resolved mongodb/mongoose issue

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-mongoose.connect(process.env.DB_URI);
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 // const { MongoClient } = require("mongodb");
 // const client = new MongoClient(process.env.DB_URI);
