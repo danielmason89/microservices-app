6c4fa0a98ba83f1821b96a5cf720d0ebb748881b
daniel_mason <danmasson0@gmail.com>
2024-11-01 13:55:18 -0400

    Add mongoose, mongodb, and nanoid to package.json git commit -m

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const { MongoClient } = require("mongodb");
-
-const client = new MongoClient(process.env.DB_URI);
-const db = client.db("urlshortener");
-const urls = db.collection("urls");
-
-const port = process.env.PORT || 3000;
-
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import express from "express";
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-const cors = require("cors");
+import cors from "cors";
+import mongoose, { Schema } from "mongoose";
+import { nanoid } from "nanoid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.DB_URI);
+
+// const { MongoClient } = require("mongodb");
+// const client = new MongoClient(process.env.DB_URI);
+// const db = client.db("urlshortener");
+// const urls = db.collection("urls");
+
+const port = process.env.PORT || 3000;
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // View Routes
-app.get("/timestamp", function (req, res) {
+app.get("/timestamp", (req, res) => {
   res.sendFile(__dirname + "/views/timestamp.html");
 });
 
-app.get("/request-header-parser", function (req, res) {
+app.get("/request-header-parser", (req, res) => {
   res.sendFile(__dirname + "/views/request-header-parser.html");
 });
 
-app.get("/url-shortener", function (req, res) {
+app.get("/url-shortener", (req, res) => {
   res.sendFile(__dirname + "/views/url-shortener.html");
 });
 
 // test API endpoint...
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   console.log({ greeting: "hello API" });
   res.json({ greeting: "hello API" });
 });
 
 // Request Header Parser Microservice
-app.get("/api/whoami", function (req, res) {
+app.get("/api/whoami", (req, res) => {
   res.json({
     ipaddress: req.remoteAddress,
     language: req.headers["accept-language"],
@@ -51,67 +61,47 @@ app.get("/api/whoami", function (req, res) {
   });
 });
 
-// URL Shortener Microservice - https://www.freecodecamp.org
-app.post("/api/shorturl", function (req, res) {
-  const client_requested_url = req.body.url;
+// URL Shortener Microservice
+let ShortURL = mongoose.model(
+  "ShortURL",
+  new Schema({ short_url: String, original_url: String, suffix: String })
+);
 
-  if (
-    !client_requested_url.includes("https://") &&
-    !client_requested_url.includes("http://")
-  ) {
-    return res.json({ error: "invalid url" });
-  }
-
+app.post("/api/shorturl", async (req, res) => {
   try {
-    client.connect();
+    let client_requested_url = req.body.url;
+    let suffix = nanoid();
 
-    let existingUrl = urls.findOne({ original_url: client_requested_url });
-
-    if (existingUrl) {
-      return res.json({
-        original_url: existingUrl.original_url,
-        short_url: existingUrl.short_url,
-      });
-    }
-
-    // If the URL is new, generate a short URL and save it to the database
-    const short_url = urls.countDocuments() + 1;
-    urls.insertOne({
+    let newURL = new ShortURL({
+      short_url: `${req.protocol}://${req.get("host")}/api/shorturl/${suffix}`,
       original_url: client_requested_url,
-      short_url: short_url,
+      suffix: suffix,
     });
 
+    newURL.save();
+
     res.json({
-      original_url: client_requested_url,
-      short_url: short_url,
+      saved: true,
+      short_url: newURL.short_url,
+      original_url: newURL.original_url,
+      suffix: newURL.suffix,
     });
   } catch (error) {
     console.error("Error handling /api/shorturl:", error);
-    res.status(500).json({ error: "Database error" });
+    res.status(500).json({ error: "invalid url" });
   }
 });
 
-app.get("api/shorturl/:shorturl", function (req, res) {
-  const shorturl = parseInt(req.params.shorturl);
-  try {
-    client.connect();
-
-    const urlRecord = urls.findOne({ short_url: shorturl });
-
-    if (!urlRecord) {
-      return res.json({ error: "No short URL found for the given input" });
-    }
-
-    // Redirect to the original URL
-    res.redirect(urlRecord.original_url);
-  } catch (error) {
-    console.error("Error handling /api/shorturl/:shorturl:", error);
-    res.status(500).json({ error: "Database error" });
-  }
+app.get("/api/shorturl/:suffix", (req, res) => {
+  let userGeneratedSuffix = req.params.suffix;
+  ShortURL.find({ suffix: userGeneratedSuffix }).then((foundUrls) => {
+    let urlForDirect = foundUrls[0];
+    res.redirect(urlForDirect.original_url);
+  });
 });
 
 // Timestamp Microservice
-app.get("/api/", function (req, res) {
+app.get("/api/", (req, res) => {
   let now = new Date();
   res.json({
     unix: now.getTime(),
@@ -119,7 +109,7 @@ app.get("/api/", function (req, res) {
   });
 });
 
-app.get("/api/:date_string", function (req, res) {
+app.get("/api/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
   if (!isNaN(dateString) && parseInt(dateString) > 10000) {
@@ -142,6 +132,6 @@ app.get("/api/:date_string", function (req, res) {
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(port, function () {
+var listener = app.listen(port, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
