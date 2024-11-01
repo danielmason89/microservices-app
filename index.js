1d680c70e4e3b4f78c21e7c7cfd63e9dec882c6d
daniel_mason <danmasson0@gmail.com>
2024-11-01 11:59:09 -0400

    added connection to mongodb completed url shortener microservice added two routes to complete this assignment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.DB_URI);
+const db = client.db("urlshortener");
+const urls = db.collection("urls");
+
 const port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.urlencoded({ extended: true }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -15,6 +23,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// View Routes
 app.get("/timestamp", function (req, res) {
   res.sendFile(__dirname + "/views/timestamp.html");
 });
@@ -23,22 +32,85 @@ app.get("/request-header-parser", function (req, res) {
   res.sendFile(__dirname + "/views/request-header-parser.html");
 });
 
-// your first API endpoint...
+app.get("/url-shortener", function (req, res) {
+  res.sendFile(__dirname + "/views/url-shortener.html");
+});
+
+// test API endpoint...
 app.get("/api/hello", function (req, res) {
   console.log({ greeting: "hello API" });
   res.json({ greeting: "hello API" });
 });
 
+// Request Header Parser Microservice
 app.get("/api/whoami", function (req, res) {
   res.json({
-    // value: Object.keys(req),
-    ipaddress: req.connection.remoteAddress,
+    ipaddress: req.remoteAddress,
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
-    // "req-headers": req.headers,
   });
 });
 
+// URL Shortener Microservice - https://www.freecodecamp.org
+app.post("/api/shorturl", function (req, res) {
+  const client_requested_url = req.body.url;
+
+  if (
+    !client_requested_url.includes("https://") &&
+    !client_requested_url.includes("http://")
+  ) {
+    return res.json({ error: "invalid url" });
+  }
+
+  try {
+    client.connect();
+
+    let existingUrl = urls.findOne({ original_url: client_requested_url });
+
+    if (existingUrl) {
+      return res.json({
+        original_url: existingUrl.original_url,
+        short_url: existingUrl.short_url,
+      });
+    }
+
+    // If the URL is new, generate a short URL and save it to the database
+    const short_url = urls.countDocuments() + 1;
+    urls.insertOne({
+      original_url: client_requested_url,
+      short_url: short_url,
+    });
+
+    res.json({
+      original_url: client_requested_url,
+      short_url: short_url,
+    });
+  } catch (error) {
+    console.error("Error handling /api/shorturl:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+app.get("api/shorturl/:shorturl", function (req, res) {
+  const shorturl = parseInt(req.params.shorturl);
+  try {
+    client.connect();
+
+    const urlRecord = urls.findOne({ short_url: shorturl });
+
+    if (!urlRecord) {
+      return res.json({ error: "No short URL found for the given input" });
+    }
+
+    // Redirect to the original URL
+    res.redirect(urlRecord.original_url);
+  } catch (error) {
+    console.error("Error handling /api/shorturl/:shorturl:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+// Timestamp Microservice
 app.get("/api/", function (req, res) {
   let now = new Date();
   res.json({
